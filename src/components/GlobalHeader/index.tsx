a1bae257a4c5cebfd4418cea1b3dcd6c221daff2
Robert Zaremba <robert.zaremba@scale-it.pl>
2019-05-05 15:03:10 +0200

    use component

--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,40 +1,37 @@
-import React, { PureComponent } from 'react'
+import React, { useEffect } from 'react'
 import { Icon } from 'antd'
 import Link from 'umi/link'
 import Debounce from 'lodash-decorators/debounce'
 import styles from './index.less'
 import RightContent from './RightContent'
 
-export default class GlobalHeader extends PureComponent {
-  componentWillUnmount () {
-    this.triggerResizeEvent.cancel()
-  }
-  /* eslint-disable*/
+export default  function GlobalHeader2 (props) {
+  // eslint-disable
   @Debounce(600)
-  triggerResizeEvent () {
-    // eslint-disable-line
+  function triggerResizeEvent () {
     const event = document.createEvent('HTMLEvents')
     event.initEvent('resize', true, false)
     window.dispatchEvent(event)
   }
-  toggle = () => {
-    this.props.onCollapse(!this.props.collapsed)
-    this.triggerResizeEvent()
+
+  function toggle () {
+    props.onCollapse(!props.collapsed)
+    triggerResizeEvent()
   }
-  render () {
-    const { collapsed, isMobile, logo } = this.props
-    return (
-      <div className={styles.header}>
-        {isMobile && (
-          <Link to='/' className={styles.logo} key='logo'>
-            <img src={logo} alt='logo' width='32' />
-          </Link>
-        )}
-        <span className={styles.trigger} onClick={this.toggle}>
-          <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} />
-        </span>
-        <RightContent {...this.props} />
-      </div>
-    )
-  }
+
+  useEffect(() => triggerResizeEvent.cancel)
+
+  return (
+    <div className={styles.header}>
+      {props.isMobile && (
+        <Link to='/' className={styles.logo} key='logo'>
+          <img src={props.logo} alt='logo' width='32' />
+        </Link>
+      )}
+      <span className={styles.trigger} onClick={toggle}>
+        <Icon type={props.collapsed ? 'menu-unfold' : 'menu-fold'} />
+      </span>
+      <RightContent {...props} />
+    </div>
+  )
 }
