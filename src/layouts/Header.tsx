9a1a2589510f606a53ed1c705dbb997dfcf1abdd
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2019-05-06 22:59:25 +0200

    changed PureComponent Header to react hooks

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import { formatMessage } from 'umi/locale'
 import { Layout, message } from 'antd'
 import Animate from 'rc-animate'
@@ -10,30 +10,27 @@ import styles from './Header.less'
 
 const { Header } = Layout
 
-class HeaderView extends PureComponent {
-  state = {
-    visible: true
-  }
+function HeaderView (props) {
+  const [visible, setVisible] = useState(true)
+  // TO Verify: set logic for oldScrollTop and ticking
+  const [oldScrollTop, setOldScrollTop] = useState(document.body.scrollTop)
+  const [ticking, setTicking] = useState(true)
 
-  static getDerivedStateFromProps (props, state) {
-    if (!props.autoHideHeader && !state.visible) {
-      return {
-        visible: true
-      }
+  useEffect(() => {
+    // componentdidmount
+    document.addEventListener('scroll', handScroll, { passive: true })
+    // Specify how to clean up after this effect:
+    return function cleanup () {
+      // componentdidunmount
+      document.removeEventListener('scroll', handScroll)
     }
-    return null
-  }
+  })
 
-  componentDidMount () {
-    document.addEventListener('scroll', this.handScroll, { passive: true })
-  }
+  const { isMobile, handleMenuCollapse, setting } = props
+  const { navTheme, layout, fixedHeader } = setting
 
-  componentWillUnmount () {
-    document.removeEventListener('scroll', this.handScroll)
-  }
-
-  getHeadWidth = () => {
-    const { isMobile, collapsed, setting } = this.props
+  const getHeadWidth = () => {
+    const { isMobile, collapsed, setting } = props
     const { fixedHeader, layout } = setting
     if (isMobile || !fixedHeader || layout === 'topmenu') {
       return '100%'
@@ -41,21 +38,31 @@ class HeaderView extends PureComponent {
     return collapsed ? 'calc(100% - 80px)' : 'calc(100% - 256px)'
   }
 
-  handleNoticeClear = type => {
+  // TODO: cant see usage
+  // const getDerivedStateFromProps = (props) => {
+  //   if (!props.autoHideHeader && !visible) {
+  //     return {
+  //       visible: true
+  //     }
+  //   }
+  //   return null
+  // }
+
+  const handleNoticeClear = type => {
     message.success(
       `${formatMessage({ id: 'component.noticeIcon.cleared' })} ${formatMessage({
         id: `component.globalHeader.${type}`
       })}`
     )
-    const { dispatch } = this.props
+    const { dispatch } = props
     dispatch({
       type: 'global/clearNotices',
       payload: type
     })
   }
 
-  handleMenuClick = ({ key }) => {
-    const { dispatch } = this.props
+  const handleMenuClick = ({ key }) => {
+    const { dispatch } = props
     if (key === 'userCenter') {
       router.push('/account/center')
       return
@@ -75,79 +82,68 @@ class HeaderView extends PureComponent {
     }
   }
 
-  handleNoticeVisibleChange = visible => {
+  const handleNoticeVisibleChange = visible => {
     if (visible) {
-      const { dispatch } = this.props
+      const { dispatch } = props
       dispatch({
         type: 'global/fetchNotices'
       })
     }
   }
 
-  handScroll = () => {
-    const { autoHideHeader } = this.props
-    const { visible } = this.state
+  const handScroll = () => {
+    const { autoHideHeader } = props
     if (!autoHideHeader) {
       return
     }
     const scrollTop = document.body.scrollTop + document.documentElement.scrollTop
-    if (!this.ticking) {
-      this.ticking = true
+    if (!ticking) {
+      setTicking(true)
       requestAnimationFrame(() => {
-        if (this.oldScrollTop > scrollTop) {
-          this.setState({
-            visible: true
-          })
+        if (oldScrollTop > scrollTop) {
+          setVisible(true)
         } else if (scrollTop > 300 && visible) {
-          this.setState({
-            visible: false
-          })
+          setVisible(false)
         } else if (scrollTop < 300 && !visible) {
-          this.setState({
-            visible: true
-          })
+          setVisible(true)
         }
-        this.oldScrollTop = scrollTop
-        this.ticking = false
+        setOldScrollTop(scrollTop)
+        setTicking(false)
       })
     }
   }
 
-  render () {
-    const { isMobile, handleMenuCollapse, setting } = this.props
-    const { navTheme, layout, fixedHeader } = setting
-    const { visible } = this.state
-    const isTop = layout === 'topmenu'
-    const width = this.getHeadWidth()
-    const HeaderDom = visible ? (
-      <Header style={{ padding: 0, width }} className={fixedHeader ? styles.fixedHeader : ''}>
-        {isTop && !isMobile ? (
-          <TopNavHeader
-            theme={navTheme}
-            mode='horizontal'
-            onCollapse={handleMenuCollapse}
-            onNoticeClear={this.handleNoticeClear}
-            onMenuClick={this.handleMenuClick}
-            onNoticeVisibleChange={this.handleNoticeVisibleChange}
-            {...this.props}
-          />
-        ) : (
-          <GlobalHeader
-            onCollapse={handleMenuCollapse}
-            onNoticeClear={this.handleNoticeClear}
-            onMenuClick={this.handleMenuClick}
-            onNoticeVisibleChange={this.handleNoticeVisibleChange}
-            {...this.props}
-          />
-        )}
-      </Header>
-    ) : null
-    return (
-      <Animate component='' transitionName='fade'>
-        {HeaderDom}
-      </Animate>
-    )
-  }
+  const isTop = layout === 'topmenu'
+  const width = getHeadWidth()
+  const HeaderDom = visible ? (
+    <Header style={{ padding: 0, width }} className={fixedHeader ? styles.fixedHeader : ''}>
+      {isTop && !isMobile ? (
+        <TopNavHeader
+          theme={navTheme}
+          mode='horizontal'
+          onCollapse={handleMenuCollapse}
+          onNoticeClear={handleNoticeClear}
+          onMenuClick={handleMenuClick}
+          onNoticeVisibleChange={handleNoticeVisibleChange}
+          {...props}
+        />
+      ) : (
+        <GlobalHeader
+          onCollapse={handleMenuCollapse}
+          onNoticeClear={handleNoticeClear}
+          onMenuClick={handleMenuClick}
+          onNoticeVisibleChange={handleNoticeVisibleChange}
+          {...props}
+        />
+      )}
+    </Header>
+  ) : null
+
+  return (
+    <Animate component='' transitionName='fade'>
+      {HeaderDom}
+    </Animate>
+  )
 }
 
 export default connect(({ user, global, setting, loading }) => ({
