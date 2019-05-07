1b3da86944758b95e7a85e78f52af2175735b250
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2019-05-07 15:23:25 +0200

    converted TopNavHeader/index.js to TopNavHeader/index.tsx 1. reference defined on div isnt clear. Marked as to review.

---
+++ src/components/TopNavHeader/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Link from 'umi/link'
+import RightContent from '../GlobalHeader/RightContent'
+import BaseMenu from '../SiderMenu/BaseMenu'
+import { getFlatMenuKeys } from '../SiderMenu/SiderMenuUtils'
+import styles from './index.less'
+
+function TopNavHeader (props: any) {
+  const [maxWidth, setMaxWidth] = useState(undefined)
+  const { theme, contentWidth, menuData, logo } = props
+  const flatMenuKeys = getFlatMenuKeys(menuData)
+
+  // getDerivedStateFromProps content
+  setMaxWidth((props.contentWidth === 'Fixed' ? 1200 : window.innerWidth) - 280 - 165 - 40)
+
+  return (
+    <div className={`${styles.head} ${theme === 'light' ? styles.light : ''}`}>
+      <div
+        // TO Verify: this main reference is not defined well
+        // ref={ref => {
+        //   this.maim = ref
+        // }}
+        className={`${styles.main} ${contentWidth === 'Fixed' ? styles.wide : ''}`}
+      >
+        <div className={styles.left}>
+          <div className={styles.logo} key='logo' id='logo'>
+            <Link to='/'>
+              <img src={logo} alt='logo' />
+              <h1 className='cow'>APnt Design Pro</h1>
+            </Link>
+          </div>
+          <div
+            style={{
+              maxWidth
+            }}
+          >
+            <BaseMenu {...props} flatMenuKeys={flatMenuKeys} className={styles.menu} />
+          </div>
+        </div>
+        <RightContent {...props} />
+      </div>
+    </div>
+  )
+}
+
+export default TopNavHeader
